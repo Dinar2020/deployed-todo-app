3546099099aa18811b59fba0cc785cad1017a42a
Dinário José Cunha Rebouças <61950316+Dinar2020@users.noreply.github.com>
2023-03-07 15:22:27 -0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import ListHearder from './components/ListHeader'
+import ListItem from './components/ListItem'
+import {useEffect, useState} from 'react'
+import Auth from './components/Auth'
+import { useCookies } from 'react-cookie'
+
+const App = ()=> {
+  const [cookies,setCookie,removeCookie] = useCookies(null)
+  const authToken= cookies.AuthToken
+  const userEmail=cookies.Email
+  const [tasks, setTasks]= useState(null)
+
+
+  const getData = async()=>{
+
+    
+    try{
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
+      const json = await response.json()
+      setTasks(json)
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
+
+  useEffect (() => {
+    if (authToken) {
+      getData()
+    }}
+  , [])
+
+  
+
+  //Sort by date
+
+  const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
+
+  return (
+    <div className="app">
+      {!authToken && <Auth/>}
+      {authToken && 
+      <>
+      <ListHearder listName={'🌴Holiday tick list'} getData={getData}/>
+      <p className="user-email">Welcome back {userEmail} </p>
+      {sortedTasks?.map( (task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+      </>}
+      <p className="copyright">© Creative Coding LLC</p>
+    </div>
+  )
+  
+}
+
+export default App
